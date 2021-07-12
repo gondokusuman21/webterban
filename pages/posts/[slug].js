8e1cd38e7eacfee9ea68c1c69384807f081292e9
Ariq Athallah <ariqathallah@hotmail.com>
2021-07-12 12:49:32 +0700

    fixed bug

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -25,19 +25,19 @@ const BlogPage = ({ title, date, content }) => {
   return (
     <>
       <div className='my-2 cursor-pointer hover:underline'>
-        <Link href='/'>
+        <Link href='/' passHref>
           <div className='flex'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              class='h-6 w-6'
+              className='h-6 w-6'
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'
             >
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M10 19l-7-7m0 0l7-7m-7 7h18'
               />
             </svg>
