a044e28143540423842649c0797b5ab2a72ff2b6
Aldy Rambe <aldyrmbe@gmail.com>
2021-07-30 20:37:15 +0700

    Add search bar

--- pages/article/index.js
+++ pages/article/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
 
 const STRAPI_URL = "https://strapi-gk.herokuapp.com";
 
@@ -62,50 +62,62 @@ const Article = ({ postsData, singlePostData, categoryData }) => {
 
       <main className="w-11/12 mx-auto mt-5 lg:flex lg:flex-row-reverse lg:w-11/12 lg:justify-between xl:w-3/4">
         <aside className="my-5 lg:mb-0 lg:mt-5 lg:w-1/4">
+          <div className="pt-2 relative w-56 text-gray-600">
+            <input className="border-2 border-gray-300 bg-white h-10 pl-2 pr-9 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="search" name="search" placeholder="Search"></input>
+            <button type="submit" className="absolute top-18px right-1">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+              </svg>
+            </button>
+          </div>
           <h4 className="text-sm font-bold mb-2">TOPIK LAINNYA</h4>
           {categoryData.map((category, index) => {
             return (
               <Link passHref href={`/category/${category.name}`} key={index}>
-                <span className="cursor-pointer py-1 px-3 text-sm mr-2 border rounded-full bg-gray-200">{category.name}</span>
+                <span className="mr-3 z-20 text-sm lg:text-base text-gray-700 hover:text-blue-500 cursor-pointer">#{category.name}</span>
               </Link>
             )
           })}
         </aside>
         <hr className="my-5 lg:hidden"></hr>
         <section className="lg:w-8/12 mt-5">
-          <div>
-            <Image
-              alt="Main image"
-              src={mainImage.url}
-              width={mainImage.width}
-              height={mainImage.height}
-              layout="responsive"
-            />
-          </div>
-          <div className="mt-3 flex flex-col gap-y-2">
-            <div>
-              <h5 className="text-xs lg:text-sm text-gray-500">Artikel oleh <span className="text-black font-bold">{singlePostData.author.username}</span></h5>
-              {
-                singlePostData.categories.map((category, index) => {
-                  return (
-                    <Link key={index} href={`/category/${category.name}`} passHref>
-                      <span className="mr-1 z-20 text-xs lg:text-sm text-gray-500 hover:text-blue-500 cursor-pointer" key={index}>#{category.name}</span>
-                    </Link>
-                  )
-                })
-              }
+          <Link passHref href={`/posts/${singlePostData.slug}`}>
+            <div className="cursor-pointer hover:scale-150">
+              <div>
+                <Image
+                  alt="Main image"
+                  src={mainImage.url}
+                  width={mainImage.width}
+                  height={mainImage.height}
+                  layout="responsive"
+                />
+              </div>
+              <div className="mt-3 flex flex-col gap-y-2">
+                <div>
+                  <h5 className="text-xs lg:text-sm text-gray-500">Artikel oleh <span className="text-black font-bold">{singlePostData.author.username}</span></h5>
+                  {
+                    singlePostData.categories.map((category, index) => {
+                      return (
+                        <Link key={index} href={`/category/${category.name}`} passHref>
+                          <span className="mr-1 z-20 text-xs lg:text-sm text-gray-500 hover:text-blue-500 cursor-pointer" key={index}>#{category.name}</span>
+                        </Link>
+                      )
+                    })
+                  }
+                </div>
+                <h3 className="font-black text-lg">{singlePostData.title}</h3>
+                <p className="text-base text-gray-600">{singlePostData.description}</p>
+                <h5 className="text-xs sm:text-sm text-gray-500">
+                  <span>{convertDate(singlePostData.date)}</span>
+                  <span> · </span>
+                  <span>{getReadingTime(singlePostData.content)} menit membaca</span>
+                </h5>
+              </div>
             </div>
-            <h3 className="font-black text-lg">{singlePostData.title}</h3>
-            <p className="text-base text-gray-600">{singlePostData.description}</p>
-            <h5 className="text-xs sm:text-sm text-gray-500">
-              <span>{convertDate(singlePostData.date)}</span>
-              <span> · </span>
-              <span>{getReadingTime(singlePostData.content)} menit membaca</span>
-            </h5>
-          </div>
+          </Link>
           <h4 className="text-sm font-bold mt-10 -mb-5">ARTIKEL LAINNYA</h4>
           {
-            postsData.map((post, index) => {
+            postsData.slice(1, postsData.length).map((post, index) => {
               let url = post.picture.formats?.large?.url || post.picture.formats?.medium?.url || post.picture.formats?.small?.url;
               return (
                 <>
