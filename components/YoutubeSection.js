c3fc35d20822deab261e0f03bbab5a7fcb23a946
Aldy Rambe <aldyrmbe@gmail.com>
2021-07-31 16:25:22 +0700

    fix responsivness

--- components/YoutubeSection.js
+++ components/YoutubeSection.js
@@ -2,11 +2,11 @@ import Fade from 'react-reveal';
 
 const YoutubeSection = () => {
   return (
-    <section className="py-20 bg-light-blue">
+    <section className="py-5 lg:py-10 bg-light-blue">
       <div className="flex w-11/12 mx-auto items-center text-lg lg:text-3xl font-bold tracking-tight">
         <h5>Kampung Jawa : Destinasi Wisata di Yogyakarta</h5>
       </div>
-      <div className="mt-5 w-11/12 mx-auto">
+      <div className="mt-2 w-11/12 mx-auto">
         <Fade big duration={1500}>
           <div className="lg:w-7/12">
             <div id="yt-video" className="aspect-w-16 aspect-h-9 border">
